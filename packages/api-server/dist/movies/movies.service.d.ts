b7ed1dacf007abf9df1b0487bfe20dd48d7cd93e
이영건 <ygun.lee@navercorp.com>
2021-01-22 11:43:14 +0900

    update gitignore

--- packages/api-server/dist/movies/movies.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Movie } from './entities/movie.entity';
-export declare class MoviesService {
-    private movies;
-    getAll(): Movie[];
-    getOne(id: number): Movie;
-    deleteOne(id: number): void;
-    createOne(movieData: any): void;
-}
--- packages/api-server/dist/users/users.service.d.ts
+++
@@ -1,5 +0,0 @@
-import { User } from './models/user.model';
-export declare class UsersService {
-    private sampleUser;
-    findOneById(id: number): User;
-}
